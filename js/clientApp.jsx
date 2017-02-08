adf3c16b60ed19b118f1ce43f9a2a5cefffe4142
Bladimir Ardiles <bladi_sys@hotmail.com>
2017-02-08 18:09:23 +0100

    Added data to redux layer

--- js/clientApp.jsx
+++ js/clientApp.jsx
@@ -1,32 +1,33 @@
 const React = require('react')
-const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Search = require('./Search')
 const Layout = require('./Layout')
 const Details = require('./Details')
-const { Router, Route, IndexRoute, hashHistory } = require('react-router')
+const { Router, Route, IndexRoute, browserHistory } = require('react-router')
 const { store } = require('./Store')
 const { Provider } = require('react-redux')
 
 
-const containerApp = document.getElementById('app');
+const myRoutes = () => (
+  <Route path='/' component={Layout}>
+    <IndexRoute component={Landing} />
+    <Route path='/search' component={Search} />
+    <Route path='/details/:id' component={Details} />
+  </Route>
+)
 
 const App = React.createClass ({
   render () {
     return (
       <Provider store={store}>
-        <Router history={hashHistory}>
-          <Route path='/' component={Layout}>
-            <IndexRoute component={Landing} />
-            <Route path='/search' component={Search} />
-            <Route path='/details/:id' component={Details} />
-          </Route>
+        <Router history={browserHistory}>
+          {myRoutes()}
         </Router>
       </Provider>
     )
   }
 });
 
-ReactDOM.render(<App/>, containerApp);
+App.Routes = myRoutes
 
 
